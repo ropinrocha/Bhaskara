739af8c07f0712a86a5b19d67a73f2e03ae349b2
ropinrocha <romulo_pbr@hotmail.com>
2023-08-14 21:55:57 -0300

    Bhaskara working

--- bhaskara.cs
+++ bhaskara.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+
+namespace Bhaskara
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split();
+            double A = double.Parse(valores[0] , CultureInfo.InvariantCulture);
+            double B = double.Parse(valores[1] , CultureInfo.InvariantCulture);
+            double C = double.Parse(valores[2] , CultureInfo.InvariantCulture);
+            double resultado1;
+            double resultado2;
+            double delta = (Math.Pow(B, 2) - (4 * A * C));
+
+
+            resultado1 = (-B + (Math.Sqrt(delta))) / (2 * A);
+            resultado2 = (-B - (Math.Sqrt(delta))) / (2 * A);
+
+            if (A != 0 && delta > 0) 
+            {
+                Console.WriteLine($"R1 = {resultado1.ToString("F5" ,CultureInfo.InvariantCulture)}\nR2 = {resultado2.ToString("F5" , CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+        }
+    }
+}
+    
